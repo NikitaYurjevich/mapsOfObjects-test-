294aaeec5f0393610a5f1d42fb9aeabebe25b152
NikitaYurjevich <80631700+NikitaYurjevich@users.noreply.github.com>
2021-05-15 21:02:14 +0500

    Add files via upload

---
+++ offices.js
@@ -0,0 +1,208 @@
+export default{
+    "country": {
+        "Russia": {
+            "cities": {
+                "Mosсow":{
+                    "coordinates": ["55.753220, 37.622513"],
+                    "offices": {
+                       "office1": {
+                        "coordinates": ["55.752096, 37.619031"],
+                        "name": "Московский Кремль",
+                        "telephone1": "+7(999) 222-22-22",
+                        "telephone2": "+7(999) 222-22-22",
+                        "managerName": "Утин Михаил Сергеевич",
+                        "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                        "coordinates": ["55.716793, 37.554532"],
+                        "name": "Лужники",
+                        "telephone1": "+7(999) 222-22-22",
+                        "telephone2": "+7(999) 222-22-22",
+                        "managerName": "Лужайкин Иван Федорович",
+                        "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Saint-Petersburg":{
+                    "coordinates": ["59.938885, 30.315294"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["59.910131, 30.377277"],
+                            "name": "Центральный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Южанин Клим Димитриевич",
+                            "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                            "coordinates": ["59.973017, 30.220557"],
+                            "name": "Газпром Арена",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Кронштадт Алексей Алексеевич",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Yekaterinburg":{
+                    "coordinates": ["56.838011, 60.597474"],
+                    "offices": {
+                       "office1": {
+                           "coordinates": ["56.840193, 60.614435"],
+                           "name": "Дом Печати",
+                           "telephone1": "+7(999) 222-22-22",
+                           "telephone2": "+7(999) 222-22-22",
+                           "managerName": "Кипелов Александр Артемович",
+                           "managerMail": "username@flagstudio.ru"
+                        },
+                       "office2": {
+                           "coordinates": ["55.716793, 37.554532"],
+                           "name": "Екатеринбург Арена",
+                           "telephone1": "+7(999) 222-22-22",
+                           "telephone2": "+7(999) 222-22-22",
+                           "managerName": "Черный Валерий Карпович",
+                           "managerMail": "username@flagstudio.ru"
+                        }
+                    }
+                },
+                "Omsk":{
+                    "coordinates": ["54.989347, 73.368221"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["54.971772, 73.284941"],
+                            "name": "Западный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Иванов Иван Иванович",
+                            "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                            "coordinates": ["54.977180, 73.509481"],
+                            "name": "Восточный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Астафьев Родинон Газманович",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Vorkuta":{
+                    "coordinates": ["67.493233, 64.049158"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["67.504805, 64.060353"],
+                            "name": "",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "",
+                            "managerName": "Лебедев Идрак Сутульевич",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                }
+            
+            }
+        },
+        "Belarus": {
+            "cities": {
+                "Minsk":{
+                    "coordinates": ["53.902207, 27.561676"],
+                    "offices": {
+                       "office1": {
+                        "coordinates": ["53.890524, 27.553685"],
+                        "name": "Галилео",
+                        "telephone1": "+7(999) 222-22-22",
+                        "telephone2": "+7(999) 222-22-22",
+                        "managerName": "Утин Михаил Сергеевич",
+                        "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                        "coordinates": ["53.895147, 27.560270"],
+                        "name": "Стадион Динамо",
+                        "telephone1": "+7(999) 222-22-22",
+                        "telephone2": "+7(999) 222-22-22",
+                        "managerName": "Лужайкин Иван Федорович",
+                        "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Mogilev":{
+                    "coordinates": ["53.894501, 30.330487"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["53.894558, 30.331969"],
+                            "name": "Центральный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Южанин Клим Димитриевич",
+                            "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                            "coordinates": ["53.853365, 30.338622"],
+                            "name": "Южный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Кронштадт Алексей Алексеевич",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Gomel":{
+                    "coordinates": ["52.424280, 31.014063"],
+                    "offices": {
+                       "office1": {
+                           "coordinates": ["52.428812, 30.905603"],
+                           "name": "Западный",
+                           "telephone1": "+7(999) 222-22-22",
+                           "telephone2": "+7(999) 222-22-22",
+                           "managerName": "Кипелов Александр Артемович",
+                           "managerMail": "username@flagstudio.ru"
+                        },
+                       "office2": {
+                           "coordinates": ["52.422307, 31.016644"],
+                           "name": "Центральный",
+                           "telephone1": "+7(999) 222-22-22",
+                           "telephone2": "+7(999) 222-22-22",
+                           "managerName": "Черный Валерий Карпович",
+                           "managerMail": "username@flagstudio.ru"
+                        }
+                    }
+                },
+                "Vitebsk":{
+                    "coordinates": ["55.184210, 30.202885"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["55.146004, 30.213351"],
+                            "name": "Лесной",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Иванов Иван Иванович",
+                            "managerMail": "username@flagstudio.ru"
+                       },
+                       "office2": {
+                            "coordinates": ["55.192281, 30.186246"],
+                            "name": "Каменный",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "+7(999) 222-22-22",
+                            "managerName": "Астафьев Родинон Газманович",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                },
+                "Pinsk":{
+                    "coordinates": ["52.111534, 26.102081"],
+                    "offices": {
+                       "office1": {
+                            "coordinates": ["52.121347, 26.084671"],
+                            "name": "Пинский",
+                            "telephone1": "+7(999) 222-22-22",
+                            "telephone2": "",
+                            "managerName": "Лебедев Идрак Сутульевич",
+                            "managerMail": "username@flagstudio.ru"
+                       }
+                    }
+                }
+            
+            }
+        }
+    }
+}
